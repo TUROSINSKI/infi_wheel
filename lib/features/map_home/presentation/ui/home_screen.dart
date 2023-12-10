acb07ef32f0a33f47ff70d72b872e97cd4a0f116
TUROSINSKI <turosinskipriv@gmail.com>
2023-12-10 15:26:14 +0100

    Added draggable sheet to home page

--- lib/features/map_home/presentation/ui/home_screen.dart
+++ lib/features/map_home/presentation/ui/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:infi_wheel/core/utils/colors.dart';
 import 'package:infi_wheel/core/utils/constants.dart';
@@ -9,11 +12,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
+        body: Stack(
+      children: <Widget>[
+        GoogleMap(
           initialCameraPosition: CameraPosition(
-        target: Location.pPolitechnikaBialostocka,
-        zoom: 17,
-      )),
-    );
+            target: Location.pPolitechnikaBialostocka,
+            zoom: 17,
+          ),
+        ),
+        DraggableScrollableSheet(
+          initialChildSize: 0.1.h,
+          minChildSize: 0.1.h,
+          maxChildSize: 0.6.h,
+          builder: (context, controller) => Material(
+            elevation: 16.0,
+            child: Container(
+              color: AppColors.kWhite,
+              child: ListView.builder(
+                controller: controller,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  final int number = index;
+                  return buildList(number);
+                },
+              ),
+            ),
+          ),
+        ),
+      ],
+    ));
   }
+
+  Widget buildList(int index) => ListTile(
+        title: Text(
+          index.toString(),
+          style: TextStyle(fontSize: 24, color: AppColors.kBlack),
+        ),
+      );
 }
