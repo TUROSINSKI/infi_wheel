8840cd50869e6c7dfcc958cab38527bc2e3ceb59
TUROSINSKI <turosinskipriv@gmail.com>
2024-01-26 23:26:46 +0100

    firebase deleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:infi_wheel/SimpleBlocObserver.dart';
 import 'package:infi_wheel/core/routes/route_config.dart';
 import 'package:infi_wheel/core/theme/theme_data.dart';
-import 'package:infi_wheel/firebase_options.dart';
 import 'package:infi_wheel/injection_container.dart';
 import 'package:infi_wheel/shared/bloc_providers.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   setupLocator();
   Bloc.observer = SimpleBlocObserver();
   runApp(const MyApp());
