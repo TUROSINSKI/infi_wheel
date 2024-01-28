af6072f30f84b5e5de4d51b97bc83be2cfe864c9
TUROSINSKI <turosinskipriv@gmail.com>
2024-01-28 15:20:26 +0100

    minor changes

--- lib/shared/bloc_providers.dart
+++ lib/shared/bloc_providers.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:infi_wheel/features/authentication/presentation/blocs/onboarding_providers/onboarding_blocs.dart';
 import 'package:infi_wheel/features/authentication/presentation/blocs/signin/signin_bloc.dart';
 import 'package:infi_wheel/features/authentication/presentation/blocs/signup/signup_bloc.dart';
 import 'package:infi_wheel/features/map_home/presentation/blocs/cars/cars_bloc.dart';
@@ -7,7 +6,6 @@ import 'package:infi_wheel/injection_container.dart';
 
 class BlocProviders {
   static List<BlocProvider> get allBlocProviders => [
-        BlocProvider<OnboardingBloc>(create: (context) => OnboardingBloc()),
         BlocProvider<CarsBloc>(create: (context) => getIt<CarsBloc>()),
         BlocProvider<SignupBloc>(create: (context) => getIt<SignupBloc>()),
         BlocProvider<SigninBloc>(create: (context) => getIt<SigninBloc>())
