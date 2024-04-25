2aa3e92d7bc307af8db8f1d672934c71aea60d2a
Turosinski MacBook <turosinskipriv@gmail.com>
2024-04-25 16:52:05 +0200

    jwt handling

--- lib/features/authentication/data/services/user_service.dart
+++ lib/features/authentication/data/services/user_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:infi_wheel/features/authentication/data/models/user_model.dart';
 
@@ -21,6 +22,8 @@ class UserService {
     }
   }
 
+  final storage = FlutterSecureStorage();
+
   Future<bool> signInUser(UserModel user) async {
     final response = await http.post(
       Uri.parse(signInUrl),
@@ -29,9 +32,18 @@ class UserService {
     );
 
     if (response.statusCode == 200) {
+      Map<String, dynamic> responseBody = json.decode(response.body);
+      String? token = responseBody['token'];
+
+      if (token != null) {
+        print(token);
+        await storage.write(key: 'jwt_token', value: token);
+      } else {
+        throw Exception('Token not found in API response');
+      }
+
       return true;
     } else {
-      print(response.statusCode);
       throw Exception('Failed to sign in user');
     }
   }
